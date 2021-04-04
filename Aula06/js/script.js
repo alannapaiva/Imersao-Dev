67d7d17118e69156d383ac2882805649db3a86ba
Alanna Paiva <alannapaiva1@hotmail.com>
2021-04-04 02:02:34 -0300

    Codigos da imersao dev

--- Aula06/js/script.js
+++ Aula06/js/script.js
@@ -1,8 +1,63 @@
-const idadeMarco = 33;
-const idadeGiizaa = 30;
-
-function sum(number1, number2){
-    console.log(number1 + number2);
+var paulo = {
+    nome: "Paulo",
+    vitorias: 2,
+    empates: 5,
+    derrotas: 1,
+    pontos: 0
+}
+var rafa = {
+    nome: "Rafa",
+    vitorias: 3,
+    empates: 5,
+    derrotas: 2,
+    pontos: 0
 }
 
-sum(idadeMarco + idadeGiizaa);
+rafa.pontos = calculaPontos(rafa)
+paulo.pontos = calculaPontos(paulo)
+
+function calculaPontos(jogador) {
+    var pontos = (jogador.vitorias * 3) + jogador.empates
+    return pontos
+}
+
+var jogadores = [rafa, paulo]
+
+exibirJogadoresNaTela(jogadores)
+
+function exibirJogadoresNaTela(jogadores) {
+    var html = ""
+    for (var i = 0; i < jogadores.length; i++) {
+        html += "<tr><td>" + jogadores[i].nome + "</td>"
+        html += "<td>" + jogadores[i].vitorias + "</td>"
+        html += "<td>" + jogadores[i].empates + "</td>"
+        html += "<td>" + jogadores[i].derrotas + "</td>"
+        html += "<td>" + jogadores[i].pontos + "</td>"
+        html += "<td><button onClick='adicionarVitoria("+ i + ")'>Vitória</button></td>"
+        html += "<td><button onClick='adicionarEmpate("+ i +")'>Empate</button></td>"
+        html += "<td><button onClick='adicionarDerrota("+ i +")'>Derrota</button></td></tr>"
+    }
+    var tabelaJogadores = document.getElementById('tabelaJogadores')
+    tabelaJogadores.innerHTML = html
+}
+
+function adicionarVitoria(i){
+  var jogador = jogadores[i]
+  jogador.vitorias++
+  jogador.pontos = calculaPontos(jogador)
+  exibirJogadoresNaTela(jogadores)
+}
+
+function adicionarEmpate(i){
+  var jogador = jogadores[i]
+  jogador.empates++
+  jogador.pontos = calculaPontos(jogador)
+  exibirJogadoresNaTela(jogadores)
+}
+
+function adicionarDerrota(i){
+  var jogador = jogadores[i]
+  jogador.derrotas++
+  //jogador.pontos = calculaPontos(jogador)
+  exibirJogadoresNaTela(jogadores)
+}
